3c89ccc7188e5bedf9a805be9a8ff7458d18d968
Vortron-rd <119626603+Vortron-rd@users.noreply.github.com>
2025-07-05 19:13:42 -0500

    Fix incorrect data type in libhpd.h.

--- libhpd.h
+++ libhpd.h
@@ -1,6 +1,6 @@
 #ifndef LIB_TDATA
 	#define LIB_TDATA
-	 int cpust(char * stat, int core); //Total clock ticks that the core has spent on a specific type of task. (set core=-1 for all cores)
+	 int cpust(int stat, int core); //Total clock ticks that the core has spent on a specific type of task. (set core=-1 for all cores)
 	 float cpustm(int core, int stat, int time); //Get Measurements over time about a stat.
 	 float cpuhz(); // Hertz the cpu is running at.
 #endif
